76eb06fd54c7bef73359e605d580277ae6349129
SaadNoor555 <ssaadn.555@gmail.com>
2022-12-01 23:31:37 +0600

    first commit

---
+++ stock_simulate.py
@@ -0,0 +1,26 @@
+import csv
+import random
+import matplotlib.pyplot as plt
+
+with open('IIM.csv', 'r') as file:
+    csvreader = csv.reader(file)
+    data = list(csvreader)
+    n = len(data)
+    prev_changes = []
+    
+    for i in range(2, n):
+        # change = (yesterday's price - today's price) / yesterday's price
+        prev_changes.append((float(data[i-1][1])-float(data[i][1]))/float(data[i-1][1]))
+    for sim in range(100):
+        last_price = float(data[n-1][1])
+        prices = []
+        for i in range(30):
+            last_price = last_price + last_price*random.choice(prev_changes)
+            prices.append(last_price)
+        # print(prices)
+        x = [ i for i in range(30)]
+        plt.plot(x, prices)
+    x = [i for i in range(30)]
+    y = [float(data[n-1][1]) for i in range(30)]
+    plt.plot(x, y, linestyle='-.')
+    plt.show()
